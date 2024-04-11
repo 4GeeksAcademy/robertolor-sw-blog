c81452a7db640470e8d9c85375ef6b5468d817c6
Roberto <51332297+Robertolor@users.noreply.github.com>
2024-04-11 18:48:56 +0000

    revision con deimian

--- src/js/component/CharacterCard.jsx
+++ src/js/component/CharacterCard.jsx
@@ -1,28 +1,43 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 
+
 export const CharacterCard = () => {
     const { store, actions } = useContext(Context)
 
+    const detailsHandler = (pageNumber) => {
+        actions.fetchCharacterDetails(pageNumber)
+        console.log(pageNumber)
+    };
+
     return (
+        
         <div className="container">
             <div className="row">
                 {store.currentCharacter.map((elem, index) => {
                     return (
                         <div key={index} className="col-md-6">
                             <div className="d-flex flex-column ms-5 me-5">
-                                
-                                    <div className="card m-3">
-                                        <img src={`https://starwars-visualguide.com/assets/img/characters/${elem.uid}.jpg`} className="card-img-top" alt="..." />
-                                        <div className="card-body">
-                                            <h5 className="card-title">{elem.name}</h5>
-                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                                        </div>
+
+                                <div className="card m-3">
+                                    <img src={`https://starwars-visualguide.com/assets/img/characters/${elem.uid}.jpg`} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{elem.name}</h5>
+                                        {store.currentCharacterDetails.map((item, i) => {
+                                            return (
+                                                <div key={i}>
+                                                    <p className="card-text">Birth Year: {item.birth_year} </p>
+                                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                </div>
+                                        )}
+
+                                        )}
+                                        <a href="#" onClick={() => detailsHandler(elem.uid)} className="btn btn-primary">More Details</a>
+
                                     </div>
-                                
+                                </div>
+
                             </div>
                         </div>
                     );
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -20,6 +20,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			
 			/* deberia haber un promise para que ejecute todas las que vienen en paralelo */
 			await getActions().fetchCharacterData()
+			await getActions().fetchCharacterDetails(1)
 
 		},
 		
@@ -31,12 +32,12 @@ const getState = ({ getStore, getActions, setStore }) => {
 				if(!response.ok){throw new Error ("Network response from API was not OK")};
 
 				let dataGeneral = await response.json();
-				console.log(dataGeneral)
+				/* console.log(dataGeneral) */
 				
 				/* initialData.push(data); */
 				setStore({...store, generalData: dataGeneral.result}) 
-				console.log(store.generalData.people)
-				console.log(`${store.generalData.people}`)
+				/* console.log(store.generalData.people)
+				console.log(`${store.generalData.people}`) */
 			}catch(error){
 				console.error("Error fetching data:", error);
 			}
@@ -52,7 +53,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				if (!response.ok){throw new Error("error fetching Character data.")};
 				
 				let dataPersonaje = await response.json();
-				console.log(dataPersonaje)
+				/* console.log(dataPersonaje) */
 				
 				/* Guardando la data de la API en el Store. Aun falta acceder al array de "Results" */
 				setStore({...store, characterData: dataPersonaje})
@@ -61,15 +62,33 @@ const getState = ({ getStore, getActions, setStore }) => {
 				
 				/* estamos guardando la data que venia DENTRO del array "results" */
 				setStore({...store, currentCharacter: currentCharactersData})
-				console.log(store.currentCharacter)
+				/* console.log(store.currentCharacter) */
 				
 			} catch(error){
 				console.error("Error fetching data2:", error)
 			}		
 		},
 
-		fetchCharacterDetails: async () => {
+		fetchCharacterDetails: async (pageNumber) => {
 			const store = getStore()
+			console.log(pageNumber)
+			
+			try{
+				let response = await fetch (`https://www.swapi.tech/api/people/${pageNumber}`)
+				if (!response.ok){throw new Error("error fetching character details.")}
+
+				let dataCharDetails = await response.json();
+				console.log(dataCharDetails.result)
+				
+				/* tenemos dudas sobre sobre su result es un array */
+				setStore({...store, currentCharacterDetails: dataCharDetails.result.properties})
+				console.log(store.currentCharacterDetails)
+
+
+			}catch (error) {
+				console.error("error fetching data 3: error")
+			}
+
 		},
 
 		}
